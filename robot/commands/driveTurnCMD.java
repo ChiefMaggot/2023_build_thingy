2a12dee96e22b7fc0a629381a95bc1c17d195111
ChiefMaggot <122415951+ChiefMaggot@users.noreply.github.com>
2023-04-05 15:46:37 -0700

    Add files via upload

---
+++ robot/commands/driveTurnCMD.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class driveTurnCMD extends CommandBase{
+    
+    private final DrivetrainSubsystem drivetrainSubsystem;
+    private final double turn;
+    private double angle;
+
+    public driveTurnCMD(DrivetrainSubsystem drivetrainSubsystem, double turn){
+        this.drivetrainSubsystem = drivetrainSubsystem;
+        this.turn = turn;
+        
+        addRequirements(drivetrainSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        drivetrainSubsystem.resetEncoders();
+        angle = turn;
+    }
+
+
+    @Override
+    public void execute(){
+        //drivetrainSubsystem.TurnPID(angle);
+        
+    }
+
+
+    @Override
+    public void end(boolean interrupted){
+        drivetrainSubsystem.tankDrive(0, 0, null);
+
+    }
+
+
+    @Override
+    public boolean isFinished(){
+        return drivetrainSubsystem.madeIt(angle);
+    }
+
+}
